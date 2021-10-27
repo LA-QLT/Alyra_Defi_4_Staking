1ce8331f163034035a696df1742e1b5469f949f5
Lahcen EL OUARDI <lahcen07elouardi@gmail.com>
2021-10-27 16:40:57 +0200

    make it better

---
+++ truffle-config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    develop: {
+      port: 7545
+    }
+  },
+  
+  // Ajouter cette partie (ou décommenter directement dans le fichier):
+  compilers: {
+    solc: {
+      version: "0.8.7", // Récupérer la version exacte de solc-bin (par défaut : la  version de truffle)
+      settings: {  // Voir les documents de solidity pour des conseils sur l'optimisation et l'evmVersion
+        optimizer: {
+        enabled: false,
+        runs: 200
+        },
+      }
+    },
+  },
+};
